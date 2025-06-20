27c28c4c82b1de24f0dc869514bc9484704b8ba0
Aditya-Goomer <goomeraddy@gmail.com>
2025-06-20 20:52:06 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Aditya.2006'
+)
+
+
+# making a cursor
+cursorObject=dataBase.cursor()
+
+
+# create database
+cursorObject.execute("CREATE DATABASE AddyCorp")
+
+print("all done")
